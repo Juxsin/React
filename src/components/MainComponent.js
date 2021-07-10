fe66d12d86bf067fa10b68e4cdce04118f377eee
Arjon Antolin <Juxsin@yahoo.com>
2021-07-10 08:55:16 -0700

    Redux Actions

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,6 +8,7 @@ import Contact from './ContactComponent';
 import About from './AboutComponents';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { addComment } from '../redux/ActionCreators';
 // import { CAMPSITES } from '../shared/campsites';
 // import { COMMENTS } from '../shared/comments';
 // import { PARTNERS } from '../shared/partners';
@@ -22,6 +23,10 @@ const mapStateToProps = state => {
     };
 };
 
+
+const mapDispatchToProps = {
+    addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text))
+};
 
 class Main extends Component {
 
@@ -40,8 +45,11 @@ class Main extends Component {
 
         const CampsiteWithId = ({match}) => {
             return (
-                <CampsiteInfo campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]} 
-                  comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)} />
+                <CampsiteInfo 
+                    campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]} 
+                    comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+                    addComment={this.props.addComment}
+                />
             );
         };
         
@@ -62,5 +70,5 @@ class Main extends Component {
     }
 }
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
 
